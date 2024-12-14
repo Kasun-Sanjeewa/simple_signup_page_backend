b3ff4095ab6715c26b64877864a04005c7476e90
Kasun3865 <ict21945@fot.sjp.ac.lk>
2024-12-15 00:39:49 +0530

    MongoDB connected

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const app = require('./app');
 const port = 3001;
 const host = 'localhost';
+const mongoose = require('mongoose');
+
+
+const uri = 'mongodb+srv://kasun3865:Kasun%401234@cluster0.ygje9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+const connect = async () => {
+    try {
+        await mongoose.connect(uri);
+        console.log("Connected to MpongoDB");
+    } catch (error) {
+        console.log('MongoDB Error', error);
+    }
+}
+
+connect()
 
 
 
