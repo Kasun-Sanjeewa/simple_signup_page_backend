137f1c2750a756641ab540bfb4eb77a7b9ea0d17
Kasun3865 <ict21945@fot.sjp.ac.lk>
2024-12-14 23:22:29 +0530

    Node server created

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const app = require('./app');
 const port = 3001;
-const host = localhost;
+const host = 'localhost';
 
 
 
+const server = app.listen(port, host, () => {
+    console.log(`Node server is listening to ${server.address().port}`)
+});
